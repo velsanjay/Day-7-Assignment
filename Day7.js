de78ddde56eab6bed4b74a362abed45165a6f788
velsanjay <121847438+velsanjay@users.noreply.github.com>
2023-03-04 12:18:53 +0530

    Update Day7.js

--- Day7.js
+++ Day7.js
@@ -12,21 +12,24 @@ request.onload = function () {
     var output=[];
      output.push(result[i].currencies)
      output=output.filter((data)=>{
-        data="USD"
-        return console.log(result[i].name.common)
+        data=="USD"
+        var usd=document.querySelector('.USD')
+
+        return usd.append(result[i].name.common+' '+' ')
      });
-       
    }
 
    //b.Get all the countries with a population of less than 2 lakhs using Filter function
    for(i in result ){
     var output=[];
     output.push(result[i].population)   
-     output=output.filter((data)=>{
-     data > 200000
-     return console.log(result[i].name.common);
-    });
+     output=output.filter((data)=>data<200000)
+    if(output.length!=0){
+        var population=document.querySelector('.population')
+        population.append(result[i].name.common+' '+' ');
+    }   
    }
+ 
    
    //c.Print the total population of countries using reduce function
    var output=[];
@@ -35,30 +38,27 @@ request.onload = function () {
    }
    var a=0;
    const sum=output.reduce((acc, curr) => acc +curr,a);
-   console.log(sum);
+   var total=document.querySelector('.total')
+   total.append(sum);
+
 
    //d.Print the following details name, capital, flag using forEach function
-   for(i in result){
-    Object.keys(result[i]).forEach(key =>{
-        console.log(result[i].name,result[i].capital,result[i].flag);
-    })
+   for(let i in result){
+    var dic=document.createElement("div")
+    dic.classList.add('san')
+    dic.innerHTML=`<div class=d><h5 class=para>${result[i].name.common}</h5><img src=${result[i].flags.png}></div>`
+    var aiv=document.querySelector('.f')
+    aiv.append(dic)
    }
 
    //e.Get all the countries from the Asia continent /region using the Filter function
-
 for(i in result){
    var output=[];
    output.push(result[i].region);
-   output=output.filter((data)=>{
-    data="Asis"
-    return console.log(result[i].name.common);
-   })
-}
-
-   
-
-
-
-
-   
+   output=output.filter((data)=>data=="Asia")
+    if(output.length!=0){
+        var asia=document.querySelector('.asia')
+        asia.append(result[i].name.common+' '+' ');
+    }
+}   
 }
